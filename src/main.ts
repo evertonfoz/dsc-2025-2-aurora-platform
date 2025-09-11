acdf6c14bcff9228b762cb43f63a15ca6de0b518
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2025-09-10 22:38:39 -0300

    feat: nest skeleton (copiado do tmp; preserva .github e README) (#7)
    * feat: nest skeleton (copiado do tmp; preserva .github e README)
    
    * chore: add lockfile for npm ci
    
    * chore(lint): fix no-floating-promises e supertest typings; add lockfile p/ npm ci
    
    * ci: detectar workspaces; usar npm run no repo simples

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+// src/main.ts
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+
+// 👇 deixa explícito que estamos ignorando a Promise
+void bootstrap();
