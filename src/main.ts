6613bcded3c07821118ba1f01dd6b96f5ecf5dd3
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2025-09-15 14:19:59 -0300

    Update main.ts (#8)

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3001);
 }
 
 // 👇 deixa explícito que estamos ignorando a Promise
