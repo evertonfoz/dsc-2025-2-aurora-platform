242e6477a426cd63cfaf1ab10d526f84376bf208
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2025-10-13 16:42:17 -0300

    fix(test): remove unused Repository imports and tidy service tests

--- test/users/services/users.service.find.spec.ts
+++ test/users/services/users.service.find.spec.ts
@@ -1,6 +1,5 @@
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { repositoryMockFactory } from '../../mocks/repository.mock';
 import { User } from '../../../src/users/entities/user.entity';
 import { UsersService } from '../../../src/users/users.service';
